d4638e7081bfb16f93ad0eab27bbbdf9bfd90e83
alexiscole99 <alexiscole@umail.ucsb.edu>
2019-09-17 15:00:22 +0200

    updated

--- Lab2/normalize.py
+++ Lab2/normalize.py
@@ -4,4 +4,13 @@ import sys
 import regex as re
 
 def tokenize(text):
+    #regex \p{Lu}.+?[\.\?\!]
+    #split by sentences
+    sentences = re.split('\p{Lu}.+?[\.\?\!]',text)
+    #remove punctuation
+    sentences = re.sub(r'[^\w\s]','',sentences)
+    sentences = re.sub(r'\_','',sentences)
+    #convert to lowercase
+    sentences = re.sub()
+    #may have to split afterwards or loop through list of sentences
     

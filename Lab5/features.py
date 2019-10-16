a2a974f90799477df3c97755e393118ddd40a00e
alexiscole99 <alexiscole@umail.ucsb.edu>
2019-10-16 14:35:08 +0200

    initial

--- Lab5/features.py
+++ Lab5/features.py
@@ -22,13 +22,41 @@ def extract_features_1(stack,queue,graph,feature_names,sentence):
         queue_word = queue[0]['form']
         queue_pos = queue[0]['postag']
 
-    features = [stack_pos,stack_word,queue_pos,queue_word,canLa,canRe]
+    features = [stack_pos,stack_word,queue_pos,queue_word,canRe,canLa]
     return features
 
-def extract_features_2(stack,queue,feature_names,sentence):
-    return 0
+def extract_features_2(stack,queue,graph,feature_names,sentence):
+    #2 elements from stack and 2 from input list, can-la, can-re
+    stack_word_0 = 'nil'
+    stack_pos_0 = 'nil'
+    stack_word_1 = 'nil'
+    stack_pos_1 = 'nil'
+    queue_word_0 = 'nil'
+    queue_pos_0 = 'nil'
+    queue_word_1 = 'nil'
+    queue_pos_1 = 'nil'
+    canLa = transition.can_leftarc(stack,graph)
+    canRe = transition.can_reduce(stack,graph)
 
-def extract_features_3(stack,queue,feature_names,sentence):
+    if stack:
+        stack_word_0 = stack[0]['form']
+        stack_pos_0 = stack[0]['postag']
+        if(len(stack)>1):
+            stack_word_1 = stack[1]['form']
+            stack_pos_1 = stack[1]['postag']
+
+    if queue:
+        queue_word_0 = queue[0]['form']
+        queue_pos_0 = queue[0]['postag']
+        if(len(queue)>1):
+            queue_word_1 = queue[1]['form']
+            queue_pos_1 = queue[1]['postag']
+
+    features = [stack_pos_0, stack_pos_1, stack_word_0, stack_word_1, queue_pos_0, queue_pos_1, queue_word_0, queue_word_1, canRe, canLa]
+    return features
+
+def extract_features_3(stack,queue,graph,feature_names,sentence):
+    #extract at least two more features, one of them being the part of speech and the word form of the word following the top of the stack in the sentence order
     return 0
 
 if __name__ == '__main__':

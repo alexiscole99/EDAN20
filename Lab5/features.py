02625c71017d20688f8173d2ab524c7c59077815
alexiscole99 <alexiscole@umail.ucsb.edu>
2019-10-20 19:20:44 +0200

    optimized extract function

--- Lab5/features.py
+++ Lab5/features.py
@@ -97,6 +97,94 @@ def extract_features_3(stack,queue,graph,feature_names,sentence):
     features = [stack_pos_0, stack_pos_1, stack_pos_2, stack_word_0, stack_word_1, stack_word_2, queue_pos_0, queue_pos_1, queue_pos_2, queue_word_0, queue_word_1, queue_word_2, canRe, canLa]
     return features
 
+def extract(stack,queue,graph,feature_names,sentence):
+    if(feature_names == ["stack_pos","stack_word","queue_pos","queue_word,canRe,canLa"]):
+        #word and pos for 1st in stack and 1st in queue, can-la, can-reduce
+        stack_word = 'nil'
+        stack_pos = 'nil'
+        queue_word = 'nil'
+        queue_pos = 'nil'
+        canLa = transition.can_leftarc(stack,graph)
+        canRe = transition.can_reduce(stack,graph)
+        if stack:
+            stack_word = stack[0]['form']
+            stack_pos = stack[0]['postag']
+        if queue:
+            queue_word = queue[0]['form']
+            queue_pos = queue[0]['postag']
+
+        features = [stack_pos,stack_word,queue_pos,queue_word,canRe,canLa]
+        return features
+    if(feature_names == ["stack_pos_0", "stack_pos_1", "stack_word_0", "stack_word_1", "queue_pos_0", "queue_pos_1", "queue_word_0", "queue_word_1", "canRe", "canLa"]):
+        #2 elements from stack and 2 from input list, can-la, can-re
+        stack_word_0 = 'nil'
+        stack_pos_0 = 'nil'
+        stack_word_1 = 'nil'
+        stack_pos_1 = 'nil'
+        queue_word_0 = 'nil'
+        queue_pos_0 = 'nil'
+        queue_word_1 = 'nil'
+        queue_pos_1 = 'nil'
+        canLa = transition.can_leftarc(stack,graph)
+        canRe = transition.can_reduce(stack,graph)
+
+        if stack:
+            stack_word_0 = stack[0]['form']
+            stack_pos_0 = stack[0]['postag']
+            if(len(stack)>1):
+                stack_word_1 = stack[1]['form']
+                stack_pos_1 = stack[1]['postag']
+
+        if queue:
+            queue_word_0 = queue[0]['form']
+            queue_pos_0 = queue[0]['postag']
+            if(len(queue)>1):
+                queue_word_1 = queue[1]['form']
+                queue_pos_1 = queue[1]['postag']
+
+        features = [stack_pos_0, stack_pos_1, stack_word_0, stack_word_1, queue_pos_0, queue_pos_1, queue_word_0, queue_word_1, canRe, canLa]
+        return features
+
+    if(feature_names == ["stack_pos_0", "stack_pos_1", "stack_pos_2", "stack_word_0", "stack_word_1", "stack_word_2", "queue_pos_0", "queue_pos_1", "queue_pos_2", "queue_word_0", "queue_word_1", "queue_word_2", "canRe", "canLa"]):
+        #extract at least two more features, one of them being the part of speech and the word form of the word following the top of the stack in the sentence order
+        stack_word_0 = 'nil'
+        stack_pos_0 = 'nil'
+        stack_word_1 = 'nil'
+        stack_pos_1 = 'nil'
+        stack_word_2 = 'nil'
+        stack_pos_2 = 'nil'
+        queue_word_0 = 'nil'
+        queue_pos_0 = 'nil'
+        queue_word_1 = 'nil'
+        queue_pos_1 = 'nil'
+        queue_word_2 = 'nil'
+        queue_pos_2 = 'nil'
+        canLa = transition.can_leftarc(stack,graph)
+        canRe = transition.can_reduce(stack,graph)
+
+        if stack:
+            stack_word_0 = stack[0]['form']
+            stack_pos_0 = stack[0]['postag']
+            if(len(stack)>1):
+                stack_word_1 = stack[1]['form']
+                stack_pos_1 = stack[1]['postag']
+                if(len(stack>2)):
+                    stack_word_2 = stack[2]['form']
+                    stack_pos_2 = stack[2]['postag']
+
+        if queue:
+            queue_word_0 = queue[0]['form']
+            queue_pos_0 = queue[0]['postag']
+            if(len(queue)>1):
+                queue_word_1 = queue[1]['form']
+                queue_pos_1 = queue[1]['postag']
+                if(len(queue)>2):
+                    queue_word_2 = queue[2]['form']
+                    queue_pos_2 = queue[2]['postag']
+
+        features = [stack_pos_0, stack_pos_1, stack_pos_2, stack_word_0, stack_word_1, stack_word_2, queue_pos_0, queue_pos_1, queue_pos_2, queue_word_0, queue_word_1, queue_word_2, canRe, canLa]
+        return features
+
 if __name__ == '__main__':
     train_file = './swedish_talbanken05_train.conll'
     test_file = './swedish_talbanken05_test_blind.conll'
